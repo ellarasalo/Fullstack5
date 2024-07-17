2a33e9d91ec9d18f50fe6904e327e0284d561533
gitelras <ella.rasalo@helsinki.fi>
2024-07-17 12:59:11 +0300

    5.2: blogilistan frontend, step2

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -14,9 +14,19 @@ const App = () => {
     blogService.getAll().then(initialBlogs => setBlogs(initialBlogs))
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (username, password) => {
     try {
       const user = await loginService.login({ username, password })
+      window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
       setUser(user)
       blogService.setToken(user.token)
     } catch (exception) {
@@ -25,6 +35,12 @@ const App = () => {
         setErrorMessage(null)
       }, 5000)
     }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogAppUser')
+    setUser(null)
+    blogService.setToken(null)
   }
 
   const addBlog = async (blogObject) => {
@@ -45,7 +61,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
