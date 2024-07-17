82c0d7470ac8d67cab4c2d74187ec912df4209ef
gitelras <ella.rasalo@helsinki.fi>
2024-07-17 14:57:35 +0300

    5.7 blogilistan frontend, step7

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleDetails = () => {
+    setDetailsVisible(!detailsVisible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} <button onClick={toggleDetails}>{detailsVisible ? 'hide' : 'view'}</button>
+      </div>
+      {detailsVisible && (
+        <div>
+          <p>{blog.url}</p>
+          <p>{blog.likes} likes <button>like</button></p>
+          {blog.user && <p>added by {blog.user.name}</p>}
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
